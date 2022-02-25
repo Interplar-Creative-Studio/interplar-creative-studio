8078371cdb333f3d8db841f68eb1341adef0622e
ALeX <ometov.sasha@mail.ru>
2022-02-25 16:17:22 +0300

    INT-17
    hover bg blur with "before" element

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 console.log('scripts/main.js');
 
-
+// МЕНЮ
 let menu_toggler = document.querySelector('.menu_toggler')
 let menu_counter = document.querySelector('.counter')
 let menu_bar     = document.querySelector('.menu')
@@ -10,3 +10,30 @@ menu_toggler.addEventListener("click", function(){
     menu_toggler.classList.toggle("active")
     menu_counter.classList.toggle("active")
 })
+
+// HOVER
+let nav_links  = document.querySelectorAll('.menu a');
+let btns       = document.querySelectorAll('.btn');
+let next_links = document.querySelectorAll('.next');
+let footer_email = document.querySelector('#footer .email');
+// let license_links = document.querySelectorAll('.license a'); // это лишнее, там слишком маленький текст
+
+menu_toggler.setAttribute('data-hover', menu_toggler.innerHTML.toUpperCase())
+footer_email.setAttribute('data-hover',footer_email.innerHTML.toUpperCase())
+
+nav_links.forEach(element => {
+    let text = element.innerHTML
+    element.setAttribute('data-hover', text.toUpperCase())
+});
+btns.forEach(element => {
+    let text = element.innerHTML
+    element.setAttribute('data-hover', text.toUpperCase())
+});
+next_links.forEach(element => {
+    let text = element.innerHTML
+    element.setAttribute('data-hover', text)
+});
+// license_links.forEach(element => {
+//     let text = element.innerHTML
+//     element.setAttribute('data-hover', text)
+// });
