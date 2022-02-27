a52aa908505f48241d592ea29d1c6b42a390fd36
ALeX <ometov.sasha@mail.ru>
2022-02-27 16:54:57 +0300

    INT-16
    fix footer logo (img->text)
    fix offsets

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,25 +3,27 @@ console.log('scripts/main.js');
 // МЕНЮ
 let menu_toggler = document.querySelector('.menu_toggler')
 let menu_counter = document.querySelector('.counter')
-let menu_bar     = document.querySelector('.menu')
+let menu_bar = document.querySelector('.menu')
 
-menu_toggler.addEventListener("click", function(){
+menu_toggler.addEventListener("click", function () {
     menu_bar.classList.toggle("active")
     menu_toggler.classList.toggle("active")
     menu_counter.classList.toggle("active")
 })
 
 // HOVER
-let intro_h1   = document.querySelector('#intro h1');
-let nav_links  = document.querySelectorAll('.menu a');
-let btns       = document.querySelectorAll('.btn');
+let intro_h1 = document.querySelector('#intro h1');
+let nav_links = document.querySelectorAll('.menu a');
+let btns = document.querySelectorAll('.btn');
 let next_links = document.querySelectorAll('.next');
 let footer_email = document.querySelector('#footer .email');
+let footer_logo = document.querySelector('.footer_logo');
 // let license_links = document.querySelectorAll('.license a'); // это лишнее, там слишком маленький текст
 
 intro_h1.setAttribute('data-hover', intro_h1.innerHTML.toUpperCase())
 menu_toggler.setAttribute('data-hover', menu_toggler.innerHTML.toUpperCase())
 footer_email.setAttribute('data-hover', footer_email.innerHTML.toUpperCase())
+footer_logo.setAttribute('data-hover', footer_logo.innerHTML.toUpperCase())
 
 nav_links.forEach(element => {
     let text = element.innerHTML
